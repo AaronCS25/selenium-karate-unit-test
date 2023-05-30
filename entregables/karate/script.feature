859e15263e0b7fa926437c1b967016bf2230ffa6
AaronCS25 <aaron.camacho@utec.edu.pe>
2023-05-29 20:35:24 -0500

    actividad-2: ok

--- entregables/karate/script.feature
+++ entregables/karate/script.feature
@@ -1,2 +1,7 @@
 Feature: API Testing SW 1 - UTEC
 Scenario:
+  * url 'https://httpbin.org/anything'
+  * request { myKey: 'myValue' }
+  * method post
+  * status 200
+  * match response.json == { myKey: 'myValue' }  
--- unittest/fizzbuzz.py
+++
@@ -1,7 +0,0 @@
-def function(fizzbuzz):
-    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
-        return "fizzbuzz"
-    elif fizzbuzz % 3 == 0:
-        return "fizz"
-    elif fizzbuzz % 5 == 0:
-        return "buzz"
